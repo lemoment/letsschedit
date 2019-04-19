5711a261c7bfa9470e006a9e20656c403ef83423
Elias Gabriel <egabriel@olin.edu>
2019-04-19 11:04:43 -0400

    Update documentation

--- source/api/app.py
+++ source/api/app.py
@@ -8,9 +8,7 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
+def home():
     return 'Hello World!'
-
-# more end points here!
 
 if __name__ == '__main__': app.run()
