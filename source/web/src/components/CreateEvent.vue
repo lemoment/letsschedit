825e3e8679836c9f017999a4807f06fa433fcca3
Riya Aggarwal <2000riya@gmail.com>
2019-04-21 18:49:33 -0400

    Added fillable boxes

--- source/web/src/components/CreateEvent.vue
+++ source/web/src/components/CreateEvent.vue
@@ -4,40 +4,31 @@
       </header>
       <div>
         <img src="../assets/clock.png" alt="Clock" id="logo">
-        <h3 class="title">LET'S SCHED IT</h3>
+        <h3 class="title" style="margin-bottom: 30px">LET'S SCHED IT</h3>
       </div>
-      <div style="clear: both;"></div>
-      <div class="page-content" style="background-color:#f2f5f9; margin-left:20px; float:left; padding:20px">
-        <p class="title" style="float:left;">Create an event named</p>
-        <p id="box" style="float:left;"> Meeting </p>
-        <p class="title" style="float:left;">on</p>
-        <p id="box" style="float:left;"> 5/16/20 </p>
-        <p class="title" style="float:left;">to</p>
-        <p id="box" style="float:left;"> 5/16/20 </p>
-        <p class="title" style="float:left;">from</p>
-        <p id="box" style="float:left;"> 7:00am </p>
-        <p class="title" style="float:left;">to</p>
-        <p id="box" style="float:left;"> 7:00pm </p>
-        <p class="title" style="float:left;">.</p>
 
+      <div class="page-content">
+        <form class="form-inline"> 
+          <label for="name">Create an event named</label>
+          <input type="text" id="name" name="name" placeholder="Name">
+          <label for="startDate">on</label>
+          <input type="text" id="startDate" name="startDate" placeholder="Start Date">
+          <label for="startDate">to</label>
+          <input type="text" id="endDate" name="endDate" placeholder="End Date">
+          <label for="startTime">from</label>
+          <input type="text" id="startTime" name="startTime" placeholder="Start Time">
+          <label for="endTime">to</label>
+          <input type="text" id="endTime" name="endTime" placeholder="End Time">
+          <label>.</label>
+          <button type="submit" style="align-self: flex-end; display:block;">
+           SCHED IT!</button>
+        </form>
      </div>
     </div>
 </template>
 
-<script>
-export default {
-  name: 'CreateEvent',
-  data () {
-	return {
-	  msg: 'Welcome to Your Vue.js App'
-	}
-  }
-}
-</script>
-
-<!-- Add "scoped" attribute to limit CSS to this component only -->
+<!-- CSS -->
 <style>
-
 html {
     font-family: 'Montserrat', sans-serif;
     font-weight: 700;
@@ -50,10 +41,15 @@ body {
   margin: 0;
   padding: 0;
 }
-.logo {
-    height: 5rem;
-    padding-top: 20px;
-    padding-left: 20px;
+.page-content{
+  background-color:#e5e5e5; 
+  margin-left:20px;
+  margin-right: 20px;
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
+  padding: 20px;
+  align-items: baseline;
 }
 h1 {
     font-size: 9rem;
@@ -74,22 +70,72 @@ h2 {
 h3 {
     font-size: 2rem;
 		text-align: left;
-		font-family: 'Montserrat';
-    font-style: normal;
+		font-family: 'Montserrat', sans-serif;
 		font-weight: bold;
 		padding-left: 30px;
     color: #000000;
 }
 p {
-  font-size: 2.5rem;
+  font-size: 3rem;
   text-align: center;
-  margin-top:-40px;
   text-align: left;
-  font-family: 'Montserrat';
-  font-style: normal;
-  font-weight: bold;
   padding-left: 10px;
+  font-family: 'Montserrat', sans-serif;
+  font-weight: bold;
   color: #000000;
+}
+
+.form-inline {
+  display: flex;
+  flex-flow: row wrap;
+ }
+.form-inline input {
+  padding: 3px 0 3px 0;
+  margin: 5px 10px 5px 10px;
+  box-sizing: border-box;
+  border: 2px solid black;
+  border-radius: 10px;
+  text-align: center;
+  color: #545454;
+  font-family: 'Montserrat', sans-serif;
+  font-weight: bold;
+  font-size: 2.5rem;
+  width: 15rem;
+  vertical-align: middle;
+}
+.form-inline label {
+  margin: 0 0 0 0;
+  font-size: 3rem;
+  text-align: center;
+  text-align: left;
+  padding-left: 10px;
+  font-family: 'Montserrat', sans-serif;
+  font-weight: bold;
+  color: #000000;
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-evenly;
+  align-content: 
+}
+.form-inline ::placeholder {
+  color: #bab4b4;
+  font-weight: normal;
+}
+.form-inline button {
+  border: 2px solid #52BDDF;
+  box-sizing: border-box;
+  border-radius: 10px;
+  background-color: #FFFFFF;
+  cursor: pointer;
+  font-size: 3rem;
+  font-family: 'Montserrat', sans-serif;
+  font-weight: bold;
+  color: #52BDDF;
+
+}
+.form-inline button:hover {
+  background-color: #52BDDF;
+  color: #FFFFFF;
 }
 .title {
     margin-top: 0px;
@@ -100,25 +146,20 @@ p {
     margin-bottom: 0px;
 
 }
-.lead {
-    margin-top: 20px;
-    margin-bottom: 20px;
-}
-#box {
-  text-align: center;
-  border-radius: 15px;
-  border: 2px solid #000000;
-  padding: 10px;
-  width: 12rem;
-  height: 3rem;
-  margin-top: -0.8rem;
-  margin-left: 1rem;
-  vertical-align: middle;
-  color: #7c8087;
-}
 #logo {
   padding-left: 30px;
   float: left;
   padding-right: 10px;
 }
 </style>
+
+<script>
+export default {
+  name: 'CreateEvent',
+  data () {
+  return {
+    msg: 'Welcome to Your Vue.js App'
+  }
+  }
+}
+</script>
