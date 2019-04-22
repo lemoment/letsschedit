837ccab6423633b255e08af93453306d83bf4368
Dieter Brehm <inkering@gmail.com>
2019-04-21 23:17:35 -0400

    setup api

--- source/web/src/main.js
+++ source/web/src/main.js
@@ -3,8 +3,19 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import VueGAPI from 'vue-gapi'
+import { APITHING, CLIENTTHING } from './secrets'
 
 Vue.config.productionTip = false
+
+const apiConfig = {
+  apiKey: APITHING,
+  clientId: CLIENTTHING + '.apps.googleusercontent.com.apps.googleusercontent.com',
+  discoveryDocs: ['https://www.googleapis.com/discovery/v1/apis/calendar/v3/rest'],
+  scope: 'https://www.googleapis.com/auth/calendar.readonly'
+}
+
+Vue.use(VueGAPI, apiConfig)
 
 /* eslint-disable no-new */
 new Vue({
