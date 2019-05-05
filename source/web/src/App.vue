5fc1456c8efeb618d61972301fbc8aa6effff1e2
Dieter Brehm <inkering@gmail.com>
2019-05-05 15:11:29 -0400

    add an express server for production running

--- source/web/src/App.vue
+++ source/web/src/App.vue
@@ -1,9 +1,9 @@
 <template>
   <div id="app">
     <div id="nav">
-      <!-- <router-link to="/">Home</router-link> -->
-      <!-- <router-link to="create-event">create-event</router-link> -->
-      <!-- <router-link to="cal">cal</router-link> -->
+      <router-link to="/">Home</router-link>
+      <router-link to="create-event">create-event</router-link>
+      <router-link to="cal">cal</router-link>
     </div>
     <div class="container">
       <router-view></router-view>
