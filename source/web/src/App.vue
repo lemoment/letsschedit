c0ee43a5df1a2f9564c53b71860e3e3ad0d69463
Dieter Brehm <inkering@gmail.com>
2019-05-04 23:01:33 -0400

    a bit of styling madness

--- source/web/src/App.vue
+++ source/web/src/App.vue
@@ -20,4 +20,48 @@ export default {
 </script>
 
 <style>
+html {
+  font-family: 'Montserrat', sans-serif;
+  font-weight: 700;
+  background-color: #FFFFFF;
+  color: #000000;
+  text-align: left;
+}
+html, body {
+  height: 100%;
+  margin: 0;
+  padding: 0;
+}
+.page-content{
+  background-color:#FFFFFF;
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: flex-start;
+  padding: 30px;
+  align-items: left;
+}
+h1 {
+    font-size: 7rem;
+		text-align: left;
+		font-family: 'Montserrat', sans-serif;
+    font-weight: 600;
+		color: #000000;
+		padding-left: 30px;
+}
+h2 {
+    font-size: 2rem;
+    text-align: left;
+    font-family: 'Montserrat', sans-serif;
+    font-weight: bold;
+    padding-left: 30px;
+    color: #000000;
+}
+h3 {
+    font-size: 1rem;
+		text-align: left;
+		font-family: 'Montserrat', sans-serif;
+		font-weight: bold;
+		padding-left: 30px;
+    color: #000000;
+}
 </style>
